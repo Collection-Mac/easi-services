d409d32c915964b99da27758de3d8d5b74319d75
Collection-Mac <119826179+Collection-Mac@users.noreply.github.com>
2023-05-01 21:54:43 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+
+//FORMULAIRE
+const container = document.getElementById('container');
+const loginButton = document.getElementById('login');
+const signupButton = document.getElementById('signup');
+
+signupButton.addEventListener('click', () => {
+    container.classList.add('panel-active');
+})
+
+loginButton.addEventListener('click', () => {
+    container.classList.remove('panel-active');
+})
